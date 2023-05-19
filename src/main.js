03fdd72728c56f12cad2b17c19a250737a99d895
jenny <qkrwodms0201@gmail.com>
2023-05-19 10:08:28 +0900

    mod

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//가장먼저 실행되는 파일
+import { createApp } from 'vue' //vue모듈에서 createApp을 가져옴,,,,
+import App from './App.vue' // App.vue파일을 가져와서 App으로 선언
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') 
+//1. App(App.vue)를 가지고 createApp을 하겠다,,,,
+//2. index.html 의 id = 'app'에 마운트
+//3. 결론 : App.vue를 id = app 에 마운트 하겟다는거 
+
